c8c5dffc7cbeae0553383c552febc9a2ae356787
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-12-09 20:31:12 +0100

    celery removed

--- src/api/routes/browser.py
+++ src/api/routes/browser.py
@@ -1,4 +1,3 @@
-import time
 from datetime import datetime
 
 from fastapi import APIRouter, HTTPException
@@ -6,7 +5,6 @@ from fastapi import APIRouter, HTTPException
 from src.config.config import MINUTES
 from src.api.dependencies import databaseDepends
 from src.models.model import Prompt
-from src.utils import celery_app
 
 router = APIRouter(prefix="/browser")
 
@@ -20,16 +18,12 @@ def start_browser(
     prompts: list[Prompt],
     domain: str,
     brand: str,
-    timeout: int = 60,
 ):
     try:
-        timestamp = int(time.time())
         # save the report
         database.add_report(
             brand_report_id, languague, country, brand, domain, datetime.now()
         )
-
-        processes = []
 
         # modify the prompt to always have domain with the name
         for prompt_data in prompts:
@@ -37,28 +31,8 @@ def start_browser(
             prompt_id = prompt_data.prompt_id
             clean_prompt = prompt.replace(brand, f"{brand}[{domain}]")
             database.update_schedule(brand_report_id, prompt_id, clean_prompt, minutes=MINUTES)
-            prompt_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            for name in ["google", "chatgpt", "perplexity"]:
-                process_id = f"{name}-{brand_report_id}-{prompt_id}-{timestamp}"
-                celery_app.run_browser.apply_async(
-                    args=(
-                        name,
-                        clean_prompt,
-                        process_id,
-                        timeout,
-                        country,
-                        brand_report_id,
-                        prompt_id,
-                        languague,
-                        brand,
-                        prompt_date,
-                    )
-                )
-                processes.append(process_id)
-
         output = {
-            "message": "Browser started",
-            "processes": processes,
+            "message": "Run scheduled",
         }
         return {"details": output}
     except Exception as e:
