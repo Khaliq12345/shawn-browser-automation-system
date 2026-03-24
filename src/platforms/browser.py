ee0a1a0a0cfc12d3dafa946aa54c2119296c8485
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2026-03-24 14:01:06 +0100

    browser is not virtual

--- src/platforms/browser.py
+++ src/platforms/browser.py
@@ -269,7 +269,7 @@ class BrowserBase(ContextDecorator, ABC):
         if HEADLESS == "yes":
             headless = True
         else:
-            headless = False
+            headless = "virtual"
         if self.country == "sg":
             proxy = {
                 "server": f"{self.get_proxy()}:{PROXY_PORT}",
